f059c4e58697d245f07ea3ccdbf834b9f906265d
Ola Fawzi <olafawzii80@gmail.com>
2024-12-19 02:53:29 +0200

    update

--- server.ts
+++ server.ts
@@ -1,13 +1,10 @@
-import 'zone.js/node';
-
-import { APP_BASE_HREF } from '@angular/common';
+import { AppServerModule } from './src/main.server';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
-import { AppServerModule } from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/test/browser');
@@ -18,7 +15,6 @@ export function app(): express.Express {
   // Log to confirm SSR rendering is occurring
   console.log('Rendering on the server!');
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
   server.engine(
     'html',
     ngExpressEngine({
@@ -28,9 +24,6 @@ export function app(): express.Express {
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
-
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
 
   // Serve static files from /browser
   server.get(
@@ -42,9 +35,7 @@ export function app(): express.Express {
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    // Log to confirm SSR route processing
-    console.log('Request received for route: ', req.url);
-
+    console.log('Request received for route: ', req.url); // Log request details
     res.render(indexHtml, {
       req,
       providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
@@ -56,17 +47,12 @@ export function app(): express.Express {
 
 function run(): void {
   const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
   const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = (mainModule && mainModule.filename) || '';
