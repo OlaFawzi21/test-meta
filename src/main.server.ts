f21b1ddd1a240d230671baac14170e6987a45212
Ola Fawzi <olafawzii80@gmail.com>
2024-12-19 03:00:26 +0200

    update

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,2 +1,11 @@
+import { enableProdMode } from '@angular/core';
+import { platformServer } from '@angular/platform-server';
+import { AppServerModule } from './app/app.server.module'; // Ensure this is correct
 
-export { AppServerModule } from './app/app.server.module';
+enableProdMode();
+
+platformServer()
+  .bootstrapModule(AppServerModule)
+  .catch((err) => console.error(err));
+export { AppServerModule };
+
